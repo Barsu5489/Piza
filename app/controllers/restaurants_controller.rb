67730396a9b731578fc1a4b93f728a5ae882ea5c
barsu5489 <ebarsulai@gmail.com>
2023-03-19 18:32:10 +0300

    tested and working get route for restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,13 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index 
+        restaurant = Restaurant.all
+        render json: restaurant
+    end
+    private
+
+    def render_not_found_respons
+        render json: { error: "Author not found" }, status: :not_found
+    end
 end
