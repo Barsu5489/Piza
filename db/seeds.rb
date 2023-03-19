a5a9fdb7b40878b940264708bb96f69f3511032f
barsu5489 <ebarsulai@gmail.com>
2023-03-19 18:18:52 +0300

    bug fix and fake data population

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require_relative '../config/application'
+require_relative '../app/models/pizza'
+require_relative '../app/models/restaurant'
+require_relative '../app/models/restaurant_pizza'
+
+restaurant1 = Restaurant.create(name: 'Pizza poa', address: 'Ngong Road')
+restaurant2 = Restaurant.create(name: 'Utamu Piza', address: 'Thika Road')
+restaurant3 = Restaurant.create(name: 'Mamma Mia Pizza', address: 'Naivasha Road')
+
+# Create some pizzas
+pizza1 = Pizza.create(name: 'chicken', ingredients: 'Tomato sauce, onions, chicken')
+pizza2 = Pizza.create(name: 'beef', ingredients: 'Pinapple, tomatoes, beef')
+pizza3 = Pizza.create(name: 'Nyamachoma', ingredients: 'chicken, cheese, Nyamachoma')
+
+# Associate pizzas with restaurants
+RestaurantPizza.create(restaurant: restaurant1, pizza: pizza1)
+RestaurantPizza.create(restaurant: restaurant1, pizza: pizza2)
+RestaurantPizza.create(restaurant: restaurant2, pizza: pizza2)
+RestaurantPizza.create(restaurant: restaurant2, pizza: pizza3)
+RestaurantPizza.create(restaurant: restaurant3, pizza: pizza1)
+RestaurantPizza.create(restaurant: restaurant3, pizza: pizza3)
